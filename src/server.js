f884bba1c3ff0e2a72ad6daa024388f0c08b5f50
Antonio <acgomesn@gmail.com>
2023-08-15 22:18:53 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/message/:id/:user', (req, res) => {
+    const { id, user } = req.params;
+
+    res.send(`
+        ID da message: ${id}
+        Para o usuário: ${user}
+    `)
+});
+
+app.get('/users', (req, res) => {
+    const { page, limit } = req.query;
+
+    res.send(`Página: ${page}. Mostrar ${limit}`)
+});
+
+
+
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
