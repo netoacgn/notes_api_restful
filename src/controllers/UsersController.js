13f776384d1cc9ee47ff20d39b8506ed8d47e1af
Antonio <acgomesn@gmail.com>
2023-08-25 11:34:37 -0300

    criptografando password ao criar usuario

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,3 +1,4 @@
+const { hash } = require('bcryptjs');
 const AppError = require('../utils/AppError')
 const sqliteConnection = require('../database/sqlite');
 
@@ -12,9 +13,10 @@ class UsersController {
             throw new AppError("Este e-mail já está em uso");
         }
 
+        const hashedPassword = await hash(password, 8)
         await database.run(
             "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-            [name, email, password]
+            [name, email, hashedPassword]
         );
 
         response.status(201).json()
